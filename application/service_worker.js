30e005b3b67dc1b316ae52e06e954c472e00e6fe
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2025-11-05 14:15:45 -0500

    Update service_worker.js

--- application/service_worker.js
+++ application/service_worker.js
@@ -10,3 +10,20 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
+
+const CACHE_NAME = "ivc-cache-v1";
+const urlsToCache = ["/", "/manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
